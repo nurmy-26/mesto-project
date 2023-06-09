dd79b301911d6a816f1a22731f0fb7ec6b79ca7b
Nurmy <nurmy26@yandex.ru>
2023-06-09 19:22:40 +0300

    Added initial cards; now multilpy close-button work

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const editBtn = document.querySelector('.btn_el_edit');
-const addBtn = document.querySelector('.btn_el_add'); // пока не понадобилась
+const addBtn = document.querySelector('.btn_el_add');
 const likeBtn = document.querySelectorAll('.btn_el_like');
-const closeBtn = document.querySelector('.btn_el_close');
+const closeBtn = document.querySelectorAll('.btn_el_close');
 const saveBtn = document.querySelector('.btn_el_save');
-const popup = document.querySelector('.popup');
+const editPopup = document.querySelector('.popup_type_profile-info'); // попап редактирования профиля
+const addPopup = document.querySelector('.popup_type_new-card'); // попап добавления новой карточки
 
 const profileName = document.querySelector('.profile__name');
 const profileDetail = document.querySelector('.profile__description');
@@ -12,37 +13,97 @@ const editForm = document.querySelector('.popup__edit-form'); // находим 
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDetail = document.querySelector('.popup__input_type_description');
 
+const cardTemplate = document.querySelector('#card').content; // выбрали шаблон с id card и сохранили его содержимое
+const gallery = document.querySelector('.gallery');
+
+// массив начальных карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// добавляем на страницу "стартовые" карточки
+function addInitialCards(name, link) {
+  const cardCopy = cardTemplate.querySelector('.card').cloneNode(true); // клонируем содержимое шаблона
+  // далее заполняем шаблон
+  cardCopy.querySelector('.card__image').alt = name;
+  cardCopy.querySelector('.card__image').src = link;
+  cardCopy.querySelector('.card__title').textContent = name;
+
+  // навешиваем возможность "лайкать"
+  cardCopy.querySelector('.btn_el_like').addEventListener('click', function(evt) {
+    evt.target.classList.toggle('js-active');
+  });
+
+  gallery.prepend(cardCopy); // добавляем заполненный шаблон в DOM (в конец gallery)
+}
+
+// пропускаем массив с карточками через forEach с применением функции, добавляющей карточки
+initialCards.forEach((item) => {
+  addInitialCards(item.name, item.link); // параметры - ключи name и link текущего элемента массива
+});
+
+// ФОРМА РЕДАКТИРОВАНИЯ ПРОФИЛЯ
 // клик на РЕДАКТИРОВАТЬ:
 function editProfile() {
-  popup.classList.add('popup_opened'); // открываем попап
+  editPopup.classList.add('popup_opened'); // открываем попап
   inputName.value = profileName.textContent; // при открытии заполняем значение поля указанным на странице
   inputDetail.value = profileDetail.textContent; // при открытии заполняем значение поля указанным на странице
 }
 
-// клик на ЗАКРЫТЬ:
-function closePopup() {
-  popup.classList.remove('popup_opened'); // закрываем попап
-}
+// клик на любую кнопку ЗАКРЫТЬ:
+closeBtn.forEach(function(item) {
+  return item.addEventListener('click', function() { // навешиваем на каждую кнопку коллекции слушатель клика
+    item.closest('.popup').classList.remove('popup_opened'); // при клике убираем класс у ближайшего родителя с классом popup
+  })
+});
 
-// клик на СОХРАНИТЬ:
+// клик на СОХРАНИТЬ (редактирование профиля):
 function confirmChanges(evt) {
     evt.preventDefault(); // отмена стандартной отправки формы
 
     profileName.textContent = inputName.value; // меняем имя на введенное
     profileDetail.textContent = inputDetail.value; // меняем подпись на введенную
 
-    popup.classList.remove('popup_opened'); // закрываем попап
+    editPopup.classList.remove('popup_opened'); // закрываем попап
 }
 
-
-
 editBtn.addEventListener('click', editProfile); // добавляем слушатель на кнопку редактирования профиля
-closeBtn.addEventListener('click', closePopup); // добавляем слушатель на кнопку закрытия попапа
 editForm.addEventListener('submit', confirmChanges); // добавляем слушатель на кнопку "сохранить"
 
 
-likeBtn.forEach(function(item) {
-  return item.addEventListener('click', function() { // навешиваем на каждую кнопку коллекции слушатель клика
-    item.classList.toggle('js-active'); // при клике сердечко меняет свой вид
-  })
-});
+// ФОРМА ДОБАВЛЕНИЯ НОВОЙ КАРТОЧКИ
+// клик на ДОБАВИТЬ:
+function addCard() {
+  addPopup.classList.add('popup_opened'); // открываем попап
+  //
+}
+
+
+// клик на СОЗДАТЬ (добавление карточки):
+
+
+addBtn.addEventListener('click', addCard); // добавляем слушатель на кнопку добавления карточки
+
