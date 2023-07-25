953e1b726e9ed9f22dafa85aa53dbf0cbc133dcc
Nurmy <nurmy26@yandex.ru>
2023-07-25 17:02:32 +0300

    feat: working on api-class

--- src/components/api.js
+++ src/components/api.js
@@ -7,6 +7,111 @@ export const config = {
   }
 }
 
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  // универсальная функция проверки ответа от сервера
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  // функция для универсального запроса с проверкой ответа
+  _request(url, options) {
+    return fetch(url, options).then(this._checkResponse)
+  }
+
+  // получение начальных карточек
+  getInitialCards() {
+    return request(`${this._baseUrl}/cards`, {
+      headers: this._headers
+    })
+  }
+
+  // загрузка информации о профиле с сервера
+  getProfileInfo() {
+    return request(`${this._baseUrl}/users/me`, {
+      headers: this._headers
+    })
+  }
+
+  // отредактировать данные профиля (имя и инфо) на сервере
+  patchProfileInfo(profileName, profileAbout) {
+    return request(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: profileName,
+        about: profileAbout
+      })
+    });
+  }
+
+  // добавить новую карточку на сервер
+  postNewCard(cardName, cardLink) {
+    return request(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: cardName,
+        link: cardLink
+      })
+    });
+  }
+
+  // удалить карточку с сервера
+  deleteCard(cardObj) {
+    return request(`${this._baseUrl}/cards/${cardObj._id}`, {
+      method: 'DELETE',
+      headers: this._headers
+    });
+  }
+
+// лайкнуть карточку на сервере
+likeCard(config, cardObj) {
+  return request(`${config.baseUrl}/cards/likes/${cardObj._id}`, {
+    method: 'PUT',
+    headers: config.headers
+  });
+}
+
+// убрать лайк на сервере
+dislikeCard(config, cardObj) {
+  return request(`${config.baseUrl}/cards/likes/${cardObj._id}`, {
+    method: 'DELETE',
+    headers: config.headers
+  });
+}
+
+// сохранить аватар на сервере
+saveAvatar(config, avatarLink) {
+  return request(`${config.baseUrl}/users/me/avatar`, {
+    method: 'PATCH',
+    headers: config.headers,
+    body: JSON.stringify({
+      avatar: avatarLink
+    })
+  });
+}
+
+
+
+
+}
+
+const api = new Api({
+  baseUrl: 'https://nomoreparties.co/v1/cohort-42',
+  headers: {
+    authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+    'Content-Type': 'application/json'
+  }
+});
+
 // загрузка информации о профиле с сервера
 export function getProfileInfo(config) {
   return request(`${config.baseUrl}/users/me`, {
@@ -15,11 +120,11 @@ export function getProfileInfo(config) {
 }
 
 // получение начальных карточек
-export function getInitialCards(config) {
-  return request(`${config.baseUrl}/cards`, {
-    headers: config.headers
-  })
-}
+// export function getInitialCards(config) {
+//   return request(`${config.baseUrl}/cards`, {
+//     headers: config.headers
+//   })
+// }
 
 // отредактировать данные профиля (имя и инфо) на сервере
 export function patchProfileInfo(config, profileName, profileAbout) {
