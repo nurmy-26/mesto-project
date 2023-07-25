145c9eda58b1bae634b976b8eedc4396ecaf7fa5
Nurmy <nurmy26@yandex.ru>
2023-07-25 18:00:30 +0300

    feat: Working on Popup Class 2

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,27 +1,37 @@
 class Popup{
-  constructor(selector){
-    this._selector = selector;
+  constructor(popupSelector){
+    this._popupSelector = popupSelector;
   }
+
   openPopup() {
-    this._selector.classList.add('popup_opened');
-    document.addEventListener('keydown', closeByEscape); // повесить обработчик Esc
+    this._popupSelector.classList.add('popup_opened');
+    document.addEventListener('keydown', this._closeByEscape); // повесить обработчик Esc
   }
+
   closePopup() {
-    this._selector.classList.remove('popup_opened');
-    document.removeEventListener('keydown', closeByEscape); // убрать обработчик Esc
+    this._popupSelector.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._closeByEscape); // убрать обработчик Esc
   }
+
   _closeByEscape(evt) {
-    if (evt.key === 'Escape') {
-      const popup = document.querySelector('.popup_opened'); // найти открытый попап по соответствующему селектору
-      this.closePopup(popup);                                     // и закрыть его
-    }
+    if (evt.key === 'Escape') this.closePopup();
+      // найти открытый попап по соответствующему селектору и закрыть его ?
+      // const popup = document.querypopupSelector('.popup_opened');
   }
-  setEventListeners(){
-    
+
+  // закрывать при клике по оверлею или крестику
+  // mousedown - чтобы не закрывалось при выделении текста и случайном отпускании за границей попапа
+  setEventListeners() {
+    this._popupSelector.addEventListener('mousedown', (evt) => {
+      if (evt.target.classList.contains('popup_opened')) {
+        this.closePopup();
+      }
+      if (evt.target.classList.contains('btn_el_close')) {
+        this.closePopup();
+      }
+    });
   }
 }
-
-
 
 
 
