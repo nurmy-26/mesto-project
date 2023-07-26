60de49b6b4e3963dadb0faa4672a43971b59bc69
Nurmy <nurmy26@yandex.ru>
2023-07-26 13:35:19 +0300

    feat: add FormValidator Class

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,12 +1,14 @@
 import Popup from './Popup'
 
-class PopupWithImage extends Popup{
-  constructor(popupSelector){
-    super(popupSelector)
+class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
     this._imageSelector = this._popupSelector.querySelector('.popup__image');
     this._descriptionSelector = this._popupSelector.querySelector('.popup__description');
+
   }
-  openPopup(link,name){
+
+  openPopup( {link, name} ) {
     this._imageSelector.src = link;
     this._imageSelector.alt = name;
     this._descriptionSelector.textContent = name;
--- src/components/validate.js
+++ src/components/validate.js
@@ -1,3 +1,123 @@
+// settings - это объект, который будем передавать в первом параметре
+export const settings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.btn_el_save',
+  inputErrorClass: 'popup__input_invalid',
+  errorClass: '.popup__error_type_'
+}
+
+class FormValidator {
+  constructor({formSelector, inputSelector, submitButtonSelector, inputErrorClass, errorClass}, formElement) {
+    this._formSelector = formSelector;
+    this._inputSelector = inputSelector;
+    this._submitButtonSelector = submitButtonSelector;
+    this._inputErrorClass = inputErrorClass;
+    this._errorClass = errorClass;
+
+    this._formElement = formElement;
+    this._inputList = Array.from(formElement.querySelectorAll(this._inputSelector))
+    this._buttonEl = formElement.querySelector(this._submitButtonSelector);
+  }
+
+  // показать ошибку
+_showError(inputEl) {
+  const errorEl = this._formElement.querySelector(`${this._errorClass}${inputEl.name}`);
+  inputEl.classList.add(this._inputErrorClass);
+  errorEl.textContent = inputEl.validationMessage;
+}
+
+// скрыть ошибку
+_hideError(inputEl) {
+  const errorEl = this._formElement.querySelector(`${this._errorClass}${inputEl.name}`);
+  inputEl.classList.remove(this._inputErrorClass);
+  errorEl.textContent = '';
+}
+
+  // проверить поле на ошибку
+_isValid(inputEl) {
+  if (inputEl.validity.patternMismatch) {
+    // если ошибка срабатывает из-за указанного в pattern, заменить текст ошибки на кастомное сообщение
+    inputEl.setCustomValidity(inputEl.dataset.customMessage);
+  } else {
+    // иначе - на пустую строку для последующего заполнения стандартным сообщением
+    inputEl.setCustomValidity('');
+  }
+
+  if (!inputEl.validity.valid) {
+    showError(inputEl);
+  } else {
+    hideError(inputEl);
+  }
+}
+
+//есть ли ошибка где-то среди полей формы
+_hasInvalidInput() {
+  return this._inputList.some(input => !input.validity.valid);
+}
+
+_disableBtn() {
+  this._buttonEl.disabled = true;
+}
+
+_enableBtn() {
+  this._buttonEl.disabled = false;
+}
+
+// вынести вердикт кнопке
+_switchBtn() {
+  if (this._hasInvalidInput()) {
+    this._disableBtn();
+  } else {
+    this._enableBtn();
+  }
+};
+
+// повесить слушатель (проверки на ошибку) на каждое поле формы
+_setValidListeners() {
+  this._inputList.forEach((input) => {
+    input.addEventListener('input', () => {
+      this._isValid(input);
+      // по мере заполнения полей выносим вердикт кнопке:
+      this._switchBtn();
+    })
+  });
+}
+
+  // проверка формы на валидность
+  // надо ли передавать форму на вход?
+enableValidation() {
+    // при 1й загрузке сайта функция деактивирует кнопки
+    this._switchBtn();
+    // вешаем слушатели ошибок
+    this._setValidListeners();
+    // вешаем на каждую форму слушатель ресета (блокирует кнопки после перезагрузки формы)
+    this._formElement.addEventListener('reset', () => {
+      this._disableBtn();
+    })
+}
+
+}
+
+// для каждой проверяемой формы создавать экземпляр класса
+
+// проверка всех форм на валидность
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  formList.forEach((form) => {
+    // создать экземпляр формы
+    // и проверить его с помощию enableValidation
+
+    // при 1й загрузке сайта функция деактивирует кнопки
+    // switchBtn(form);
+
+    // вешаем слушатели ошибок
+    // setValidListeners(form, settings);
+
+    // вешаем на каждую форму слушатель ресета (блокирует кнопки после перезагрузки формы)
+    // ...
+  })
+
+/*
 export const settings = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -89,3 +209,4 @@ export function enableValidation(settings) {
     })
   })
 }
+*/
